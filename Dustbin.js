d632467961f86a8d48e399ed423690f7ae17822a
Hritish5 <70857511+Hritish5@users.noreply.github.com>
2020-10-24 10:03:41 -0400

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,23 @@
+class dustbin {
+    constructor(x, y,width,thickness) {
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = 100;
+      this.thickness = thickness;
+      var options = {
+        isStatic: true
+      }
+      this.bottombody = Bodies.rectangle(this.x,this.y,this.width,this.thickness,options);
+      World.add(world, this.bottombody);
+    }
+    display(){
+      var bottompos = this.bottombody.position;
+      strokeWeight(6);
+      stroke("Green");
+      fill("Red");
+      rect(bottompos.x, bottompos.y,this.width,this.thickness);
+      pop();
+    }
+  };
+  
